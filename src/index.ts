743d0bced066901dc5f0926249b6b98d4bf6309a
binoy barnabas <binoybarnabas@gmail.com>
2025-07-14 12:30:21 +0530

    Vercel fix

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import userRoutes from './routes/user.routes';
 import authRotes from './routes/auth.routes'
 import propertyRoutes from './routes/property.routes';
+import serverless from 'serverless-http';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -23,3 +24,5 @@ app.use('/api/property',propertyRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export const handler = serverless(app);
